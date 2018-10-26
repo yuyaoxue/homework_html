2df99d70dbe05c65157ae0006b37bb91ed699d3c
yuyaoxue <972934562@qq.com>
2018-10-26 12:43:37 +0800

    添加 mini 图，优化网页（分离 js 脚本与 html）

--- test_javascript/scripts/ShowPic.js
+++ test_javascript/scripts/ShowPic.js
@@ -1,16 +1,43 @@
 function showPic(whichPic)
 {
+  if(!document.getElementById("placeHolder")) return false;
   var source = whichPic.getAttribute("href");
   var placeHolder = document.getElementById("placeHolder");
-  placeHolder.setAttribute("src",source);
+  if(placeHolder.nodeName != "IMG") return false;
+  placeHolder.setAttribute("src",source);// DOM Core 语句 
+  //placeHolder.src = source;//HTML DOM 语句 等同上一行语句
 
-  var text = whichPic.getAttribute("title");
+if(document.getElementById("description"))
+{
+	var text = whichPic.getAttribute("title");
   var description = document.getElementById("description");
-  description.firstChild.nodeValue = text;
+  if(description.firstChild.nodeType == 3)
+     {
+     	description.firstChild.nodeValue = text;
+     }
+}
+  return true;
 }
 
 function countBodyChildren()
 {
 	var body_element = document.getElementsByTagName("body")[0];
 	alert(body_element.childNodes.length);
+}
+function prepareGallery()
+{
+	if(!document.getElementsByTagName) return false;
+	if(!document.getElementById) return false;
+	if(!document.getElementById("imagegallery"))return false;
+
+	var gallery = document.getElementById("imagegallery");
+    var links = gallery.getElementsByTagName('a');
+   // alert("gallery.length:"+links.length);
+	for(var i = 0;i<links.length;i++)
+	{
+         links[i].onclick = function()
+         {
+         	return !showPic(this);
+         }
+	}
 }
